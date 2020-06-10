3dba51e0bf5115f824f221656295771b24baf9a0
Dongbox <sfreebobo@163.com>
2020-06-10 22:32:56 +0800

    Update task6 and usage in README.md.

--- task6.py
+++ task6.py
@@ -53,11 +53,11 @@ def task6():
         # LFinger -1.00E-06 1.5708 0.5
     """
     # 1.切换到对应场景
-    # scene_task6_PushCart()
+    scene_task6_PushCart()
     # 2.1 获取所需的控制器
     WxContrApi = WxWebotsApi.WxWebotsControllers()
     ws = WxWebotsApi.WxWebotsStepFunc()
-    # ws.standing()
+    ws.stop()
     rl_controller = WxContrApi.publish_rightLimb_controller
     rh_controller = WxContrApi.publish_rightHand_controller
     ll_controller = WxContrApi.publish_leftLimb_controller
@@ -68,8 +68,8 @@ def task6():
     l_names = ['' for _ in range(7)]
     r_names = ['' for _ in range(7)]
 
-    lh_names = ['', '', '', '', '', '', '', '', '', '']
-    rh_names = ['', '', '', '', '', '', '', '', '', '']
+    lh_names = ['' for _ in range(10)]
+    rh_names = ['' for _ in range(10)]
 
     l_command_1 = [0, 0, -1.65, -1.8, 2, 0, 0]  # 中间过程 1
     l_command_2 = [0.1, 0, -1.2, -1.85, 2.38, 0, 0]  # 中间过程 2
@@ -86,7 +86,7 @@ def task6():
     wj.sub_rightHand_joint_states()
     wj.sub_leftLimb_joint_states()
     wj.sub_rightLimb_joint_states()
-
+    #
     lh_command = [-0.0005440629177677493, 0.2,
                   0.7, 0.8,
                   0.7, 0.8,
@@ -98,86 +98,67 @@ def task6():
                   0.7, 0.8,
                   0.7, 0.8,
                   0.7, 0.8]
-    is_status_1 = False
-    is_status_2 = False
     is_status_3 = False
     is_status_4 = False
     is_status_5 = False
-    status = 1
+    status = 3
     is_l_over = False
     is_r_over = False
-    l_commands = WxWebotsApi.line_fit(l_commands, timeline=1000000, is_plot=True)
-    r_commands = WxWebotsApi.line_fit(r_commands, timeline=1000000)
-    while not rospy.is_shutdown():
-        if not is_l_over and not is_r_over:
-            for l_cmd, r_cmd in zip(l_commands, r_commands):
-                ll_controller(l_names, l_cmd)
-                rl_controller(r_names, r_cmd)
-    # try:
+    l_commands = WxWebotsApi.line_fit(l_commands, timeline=200000)
+    r_commands = WxWebotsApi.line_fit(r_commands, timeline=200000)
+    try:
+        while not rospy.is_shutdown():
+            if not is_l_over and not is_r_over:
+                for l_cmd, r_cmd in zip(l_commands, r_commands):
+                    ll_controller(l_names, l_cmd)
+                    rl_controller(r_names, r_cmd)
+                is_l_over = True
+                is_r_over = True
+
+            # 2.3 发布到相应的topic
+            if status == 4:
+                lh_controller(lh_names, lh_command)
+                rh_controller(rh_names, rh_command)
+            elif status == 5 and not is_status_5:
+                is_status_5 = True
+                status = 6
+                ws.start()  # 向前走
+                linear_x = 0.1
+                linear_y = 0.005
+                angular_z = 0.005
+                ws.vel(linear_x, linear_y, angular_z)  # 设置前向速度
+            elif status == 6:
+                if ws.cur_step_num - ws.last_step_num >= 15:
+                    ws.stop()
+                    break
+
+            leftHand_joint_states = wj.leftHand_joint_states
+            rightHand_joint_states = wj.rightHand_joint_states
+            leftLimb_joint_states = wj.leftLimb_joint_states
+            rightLimb_joint_states = wj.rightLimb_joint_states
+
+            if leftLimb_joint_states and rightLimb_joint_states:
+                l_position = list(leftLimb_joint_states.position)
+                r_position = list(rightLimb_joint_states.position)
+                if is_check_position_same(l_position, l_command_3) and \
+                        is_check_position_same(r_position, r_command_3) and \
+                        not is_status_3:
+                    status = 4
+                    is_status_3 = True
+
+            if leftHand_joint_states and rightHand_joint_states:
+                l_position = list(leftHand_joint_states.position)
+                r_position = list(rightHand_joint_states.position)
+
+                if is_check_position_same(l_position, lh_command) and \
+                        is_check_position_same(r_position, rh_command) and \
+                        not is_status_4:
+                    status = 5
+                    is_status_4 = True
+                    time.sleep(3)
     #
-    #
-    #         # 2.3 发布到相应的topic
-    #         # if status == 1:
-    #         #     ll_controller(l_names, l_command_1)
-    #         #     rl_controller(r_names, r_command_1)
-    #         # elif status == 2:
-    #         #     ll_controller(l_names, l_command_2)
-    #         #     rl_controller(r_names, r_command_2)
-    #         # elif status == 3:
-    #         #     ll_controller(l_names, l_command_3)
-    #         #     rl_controller(r_names, r_command_3)
-    #         # elif status == 4:
-    #         #     lh_controller(lh_names, lh_command)
-    #         #     rh_controller(rh_names, rh_command)
-    #         # elif status == 5 and not is_status_5:
-    #         #     is_status_5 = True
-    #         #     status = 6
-    #         #     ws.start()  # 向前走
-    #         #     linear_x = 0.1
-    #         #     linear_y = 0.005
-    #         #     angular_z = 0.005
-    #         #     ws.vel(linear_x, linear_y, angular_z)  # 设置前向速度
-    #         # elif status == 6:
-    #         #     if ws.cur_step_num - ws.last_step_num >= 15:
-    #         #         ws.stop()
-    #         #         break
-    #         #
-    #         # leftHand_joint_states = wj.leftHand_joint_states
-    #         # rightHand_joint_states = wj.rightHand_joint_states
-    #         # leftLimb_joint_states = wj.leftLimb_joint_states
-    #         # rightLimb_joint_states = wj.rightLimb_joint_states
-    #         #
-    #         # if leftLimb_joint_states and rightLimb_joint_states:
-    #         #     l_position = list(leftLimb_joint_states.position)
-    #         #     r_position = list(rightLimb_joint_states.position)
-    #         #     if is_check_position_same(l_position, l_command_1) and \
-    #         #             is_check_position_same(r_position, r_command_1) and \
-    #         #             not is_status_1:
-    #         #         status = 2
-    #         #         is_status_1 = True
-    #         #     elif is_check_position_same(l_position, l_command_2) and \
-    #         #             is_check_position_same(r_position, r_command_2) and \
-    #         #             not is_status_2:
-    #         #         status = 3
-    #         #         is_status_2 = True
-    #         #     elif is_check_position_same(l_position, l_command_3) and \
-    #         #             is_check_position_same(r_position, r_command_3) and \
-    #         #             not is_status_3:
-    #         #         status = 4
-    #         #         is_status_3 = True
-    #         #
-    #         # if leftHand_joint_states and rightHand_joint_states:
-    #         #     l_position = list(leftHand_joint_states.position)
-    #         #     r_position = list(rightHand_joint_states.position)
-    #         #     if is_check_position_same(l_position, lh_command) and \
-    #         #             is_check_position_same(r_position, rh_command) and \
-    #         #             not is_status_4:
-    #         #         status = 5
-    #         #         is_status_4 = True
-    #         #         time.sleep(3)
-    #
-    # except Exception:
-    #     pass
-    # finally:
-    #     ws.stop()
-    #     ws.standing()
+    except Exception:
+        pass
+    finally:
+        ws.stop()
+        ws.standing()
