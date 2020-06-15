4ebee47e110ac605800c19e20ad35111b575ffa5
YLD2306 <163157556@qq.com>
2020-06-15 12:02:12 +0800

    update task2.py

--- task2.py
+++ task2.py
@@ -2,183 +2,120 @@
     切换到task2 -> 拿饮料罐  20分
 
     运动控制模式
-    
+
     1、房间2放置有1张桌子，上面有5个体积大小相同但罐身装饰不同的饮料罐。
     2、本任务要求，Walker需从中拿出指定的饮料罐离开桌面并明显高于其他饮料罐保持3秒或以上。
     3、调用GraspCup任务，Walker位于房间2的固定位置，桌子与Walker的相对位置固定，Walker无需移动足部位置即可接触到饮料罐，可通过运动规划手臂完成任务。
     4、任务2的总运行时间不超1分钟，逾时则任务挑战失败。
 
-    Author: Daliang
+    Author: DaLiang
 """
 import rospy
 from utils.walker_scenes import scene_task2_GraspCup
-from utils.walker_tops_srvs import BuildConnect, WalkerWebotsSub
 from utils import WxWebotsApi
-from utils.walker_types import JointCommand
 import numpy as np
+import time
 
-count = 0
+WxContrApi = WxWebotsApi.WxWebotsControllers()
+# 左手臂控制器
+ll_controller = WxContrApi.publish_leftLimb_controller
+# 右手臂控制器
+lh_controller = WxContrApi.publish_leftHand_controller
 
-def is_check_position_same(cur_position, check_position):
-    cur_p = np.array(list(cur_position))
-    check_p = np.array(list(check_position))
-    res = abs(check_p - cur_p)
-    if sum(res<=0.004) >= 7:
-        global count
-        count += 1
-        if count == 100:
-            count = 0
-            return True
-    return False
+"""
+    左手臂参数
+    X 轴: 红色      Y 轴: 绿色      Z 轴: 蓝色  
 
-def task2(cup_num=1):
-    # 切换到任务2场景
-    scene_task2_GraspCup()
+    LShoulderPitch  -0.785  3.14        96.2    # 肩 举起/放下臂
+    LShoulderRoll   -1.57   0.0175      96.2    # 肩 打开/关闭臂
+    LShoulderYaw    -1.92   1.92        96.2    # 肩 旋转臂
 
-    WxCotroApi = WxWebotsApi.WxWebotsControllers()
-    # 获得左右手控制器
-    ll_controller = WxCotroApi.publish_leftLimb_controller
-    rl_controller = WxCotroApi.publish_rightLimb_controller
+    LElbowRoll      -2.27   1.00E-06    96.2    # 肘 打开/关闭肘
+    LElbowYaw       -2.36   2.36        27.3    # 肘 旋转肘
 
-    # 获得左右手指的控制器
-    lh_controller = WxCotroApi.publish_leftHand_controller
-    rh_controller = WxCotroApi.publish_rightHand_controller
+    LWristPitch     -0.351  0.351       27.3    # 腕 上/下切
+    LWristRoll      -0.351  0.351       27.3    # 腕 手心/背翻
+"""
+ll_names = ['LShoulderPitch', 'LShoulderRoll', 'LShoulderYaw',
+            'LElbowRoll', 'LElbowYaw',
+            'LWristPitch', 'LWristRoll']
+
+"""
+    左手指参数
+    LFinger         -1.00E-06   1.5708      0.5
+
+    LFirstFinger1
+    LFirstFinger2
+
+    LSecondFinger1
+    LSecondFinger2
+
+    LThirdFinger1
+    LThirdFinger2
+
+    LForthFinger1
+    LForthFinger2
+
+    LFifthFinger1
+    LFifthFinger2
+"""
+lh_names = ['LFirstFinger1', 'LFirstFinger2', 'LSecondFinger1', 'LSecondFinger2'
+            'LThirdFinger1', 'LThirdFinger2', 'LForthFinger1', 'LForthFinger2',
+            'LFifthFinger1', 'LFifthFinger2']
+
+lh_command = [0, 0,
+              0, 0,
+              0, 0,
+              0, 0,
+              0, 0]
 
 
-    """
-        X 轴: 红色      Y 轴: 绿色      Z 轴: 蓝色  
+def capture_cup_1():
+    ll_command_1 = [0, 0, 0, 0, 0, 0, 0]
+    ll_command_2 = [0, -1.0, 0, 0, 0, 0, 0]
+    ll_command_3 = [0, -1.0, -1.5, 0, 0, 0, 0]
+    ll_command_4 = [0, -1.0, -1.5, -1.2, 0, 0, 0]
+    ll_command_5 = [0, -0.5, -1.5, -1.2, 0.85, 0, 0]
+    ll_command_6 = [0.2, -0.5, -1.5, -1.1, 0.85, 0, 0]
 
-        LShoulderPitch  -0.785  3.14        96.2    # 肩 举起/放下臂
-        LShoulderRoll   -1.57   0.0175      96.2    # 肩 打开/关闭臂
-        LShoulderYaw    -1.92   1.92        96.2    # 肩 旋转臂
+    ll_command_7 = [0.5, -0.5, -1.5, -1.1, 0.85, 0, 0]
+    commands = [ll_command_1, ll_command_2, ll_command_3, ll_command_4, ll_command_5, ll_command_6]
+    ll_commands = WxWebotsApi.line_fit(commands, timeline=1000000)
 
-        LElbowRoll      -2.27   1.00E-06    96.2    # 肘 打开/关闭肘
-        LElbowYaw       -2.36   2.36        27.3    # 肘 旋转肘
+    lh_command0 = [0, 1.0, 0, 0, 0, 0, 0, 0, 0, 0]
+    lh_command1 = [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7]
 
-        LWristPitch     -0.351  0.351       27.3    # 腕 上/下切
-        LWristRoll      -0.351  0.351       27.3    # 腕 手心/背翻
-    """
+    lh_command2 = [0.2, 0.2, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0, 0]
 
-    # 定义参数, 参数格式可查看函数获取
-    # 左臂参数
-    l_names = ['LShoulderPitch', 'LShoulderRoll', 'LShoulderYaw',
-               'LElbowRoll', 'LElbowYaw',
-               'LWristPitch', 'LWristRoll']
-    l_command_0 = [-2.7476229711559662e-05, 0.000548553138199625, 4.0090237114409884e-06,
-                   -0.0002707919077573215, 1.6172207976904167e-06,
-                   -1.6360476324464147e-05, 0.00010110325467476545]
-    l_command_1 = [-0.7, -1.0, -1.3, 0, 0, -1.6360476324464147e-05, 0.00010110325467476545]
-    l_command_2 = [-0.7, -1.0, -1.3, -1.5, 0, -1.6360476324464147e-05, 0.00010110325467476545]        
-    l_command_3 = [0.5, -0.8, -1.3, -1.0, 0.5, -1.6360476324464147e-05, 0.00010110325467476545]
-    l_command_4 = [0.4, -0.5, -1.3, -1.0, 0.5, -1.6360476324464147e-05, 0.00010110325467476545]
-    l_command_5 = [0.48, -0.38, -1.3, -0.9, 0.9, -1.6360476324464147e-05, 0.00010110325467476545]
-    l_command_6 = [0.50, -0.35, -1.3, -0.85, 0.9, -1.6360476324464147e-05, 0.00010110325467476545]
-    l_commands = [l_command_1, l_command_2, l_command_3, l_command_4,
-                  l_command_5, l_command_6]
+    lh_command_s = [lh_command0]
+    lh_commands = WxWebotsApi.line_fit(commands=lh_command_s, timeline=100000)
 
-    # 定义标签
-    is_l_over = False
-    is_r_over = False
-    ll_commands = WxWebotsApi.line_fit(l_commands, timeline=1000000, is_plot=False)
+    ll_is_over = False
+    lh_is_over = False
     while not rospy.is_shutdown():
-        if not is_l_over and not is_r_over:
-            # 发布到topic
-            for ll_cmd in ll_commands:
-                ll_controller(l_names, ll_cmd)
-            is_l_over = True
-            is_r_over = True
+        if not ll_is_over:
+            for cmd in ll_commands:
+                ll_controller(ll_names, cmd)
+            ll_is_over = True
+        elif not lh_is_over:
+            for cmd in lh_commands:
+                lh_controller(lh_names, cmd)
+            lh_is_over = True
+        else:
+            # lh_controller(lh_names, lh_command2, mode=7)
+            # ll_controller(ll_names, ll_command_7, mode=7)
+            break
 
-    # 左手指参数
-    """
-        LFinger         -1.00E-06   1.5708      0.5
 
-        LFirstFinger1
-        LFirstFinger2
-
-        LSecondFinger1
-        LSecondFinger2
-
-        LThirdFinger1
-        LThirdFinger2
-
-        LForthFinger1
-        LForthFinger2
-
-        LFifthFinger1
-        LFifthFinger2
-    """
-    lh_names = ['LFirstFinger1', 'LFirstFinger2', 'LSecondFinger1', 'LSecondFinger2'
-                'LThirdFinger1', 'LThirdFinger2', 'LForthFinger1', 'LForthFinger2', 'LFifthFinger1', 'LFifthFinger2']
-
-    lh_command_1 = [0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0]
-
-    lh_mode = 5
-
-    wj = WxWebotsApi.WxWebotsJoints()
-    wj.sub_leftHand_joint_states()
-    wj.sub_leftLimb_joint_states()
-
-    is_status_1 = True
-    is_status_2 = False
-    is_status_3 = False
-    is_status_4 = False
-    is_status_5 = False
-    is_status_6 = False
-    is_status_7 = False
-    is_status_8 = False
-    status = 1
-    
+def capture_cup(cup_num):
     if cup_num == 1:
-        while True:
-            if status == 1:
-                ll_controller(names=l_names, command=l_command_1)
-            elif status == 2:
-                ll_controller(names=l_names, command=l_command_2)
-            elif status == 3:
-                ll_controller(names=l_names, command=l_command_3)
-            elif status == 4:
-                ll_controller(names=l_names, command=l_command_4)
-            elif status == 5:
-                ll_controller(names=l_names, command=l_command_5)
-            elif status == 6:
-                ll_controller(names=l_names, command=l_command_6)
-            elif status == 7:
-                break
+        print('Take the first cup')
+        capture_cup_1()
+    else:
+        print(str(cup_num) + ': The cup can does not exist!')
 
-            leftLimb_joint_states = wj.leftLimb_joint_states
-            leftHand_joint_states = wj.leftHand_joint_states
 
-            if leftLimb_joint_states:
-                position = list(leftLimb_joint_states.position)
-                if is_check_position_same(position, l_command_1) and is_status_1:
-                    status = 2
-                    is_status_1 = False
-                    is_status_2 = True
-                    print('完成第1步动作')
-                elif is_check_position_same(position, l_command_2) and is_status_2:
-                    status = 3
-                    is_status_2 = False
-                    is_status_3 = True
-                    print('完成第2步动作')
-                elif is_check_position_same(position, l_command_3) and is_status_3:
-                    status = 4
-                    is_status_3 = False
-                    is_status_4 = True
-                    print('完成第3步动作')
-                elif is_check_position_same(position, l_command_4) and is_status_4:
-                    status = 5
-                    is_status_4 = False
-                    is_status_5 = True
-                    print('完成第4步动作')
-                elif is_check_position_same(position, l_command_5) and is_status_5:
-                    status = 6
-                    is_status_5 = False
-                    is_status_6 = True
-                    print('完成第5步动作')
-                elif is_check_position_same(position, l_command_6) and is_status_6:
-                    status = 7
-                    is_status_6 = False
-                    is_status_7 = True
-                    print('完成第6步动作')
-            
+def task2(cup_num):
+    scene_task2_GraspCup()
+    time.sleep(4)
+    capture_cup(cup_num=cup_num)
