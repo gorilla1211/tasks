76b585110c9bf50a2e7f5eb5c97d5e87afd7fb13
John1998 <644128256@qq.com>
2020-06-18 13:38:42 +0800

    task10 unfinshied3

--- task10.py
+++ task10.py
@@ -51,14 +51,16 @@ def task10():
     rl_command0 = [0, 10, 0, -10, 0, 0, 5]
     rl_command0_0 = [-0.8891579791007349, -1.0053011435310661, 0.6744789912475938, -1.3838224546750395,
                      -0.058873321286748104, -0.06892831649295111, 0.8230945483382753]
-
+    rl_command0_1 = [-0.8891579791007349, -1.0053011435310661, 0.6744789912475938, -0.8838224546750395,
+                     -0.058873321286748104, -0.06892831649295111, 0.8230945483382753]
     rh_command0 = [0.0, 0.0,
                    0.4, 0.4,
                    0.4, 0.4,
                    0.4, 0.4,
                    0.4, 0.4]
     rl_commands = [rl_command1, rl_command2, rl_command3, rl_command4]
-
+    rl_commands2 = [rl_command0_1, rl_command1]
+    rl_command_bezier2 = WxWebotsApi.line_fit(rl_commands2, timeline=500000)
     rl_command_bezier = WxWebotsApi.line_fit(rl_commands, timeline=1500000)
 
     rl_is_over = False
@@ -67,6 +69,7 @@ def task10():
     rh2_is_over = False
     rh3_is_over = False
     rh4_is_over = False
+    rh5_is_over = False
     if ws.walk_forward_step(step_num=1, linear_x=-0.30):
         while not rospy.is_shutdown():
             # l_command[2] = 1.85  # LShoulderPitch
@@ -122,6 +125,9 @@ def task10():
                     rl_controller(rl_names, rl_command0_0, mode=5)
                 # if ws.walk_forward_step(step_num=1,linear_x=-0.01):
                 rh4_is_over = True
+            elif not rh5_is_over:
+                for cmd in rl_command_bezier2:
+                    rl_controller(rl_names, cmd, mode=5)
             else:
                 break
             # for i in range(1,300000):
