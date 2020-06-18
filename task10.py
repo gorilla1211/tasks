d55364e93b698a3bdc092624775f32bf81fae666
John1998 <644128256@qq.com>
2020-06-18 23:30:01 +0800

    task10 unfinshied6

--- task10.py
+++ task10.py
@@ -12,6 +12,7 @@ def task10():
     WxContrApi = WxWebotsApi.WxWebotsControllers()
     rl_controller = WxContrApi.publish_rightLimb_controller
     rh_controller = WxContrApi.publish_rightHand_controller
+    ll_controller = WxContrApi.publish_leftLimb_controller
     rl_names = ['RShoulderPitch', 'RShoulderRoll', 'RShoulderYaw',
                 'RElbowRoll', 'RElbowYaw',
                 'RWristPitch', 'RWristRoll']
@@ -43,11 +44,11 @@ def task10():
     rl_command3 = [-1.2, -0.4, 0.1, 0, 0, 0, 0]
     rl_command4 = [-1.57, 0.1, 0.1, 0, 0.2, 0, 0]
 
-    rh_command_force = [0.5, 0.3,
-                        0.5, 0.3,
-                        0.5, 0.3,
-                        0.5, 0.3,
-                        0.5, 0.3]
+    ll_command1 = [0, 0, 0, 0, 0, 0, 0]
+    ll_command2 = [1.5, 0.3, 0, 0, 0, 0, 0]
+    ll_command3 = [1.5, 0.3, 0, -0.65, 0, 0, 0]
+    ll_command_force = [0, 0, 0, -5, 0, 0, 0]
+
     rl_command0 = [0, 10, 0, -10, 0, 0, 5]
     rl_command0_0 = [-0.8891579791007349, -1.0053011435310661, 0.6744789912475938, -1.4038224546750395,
                      -0.058873321286748104, -0.06892831649295111, 0.8230945483382753]
@@ -75,8 +76,10 @@ def task10():
 
     rl_commands = [rl_command1, rl_command2, rl_command3, rl_command4]
     rl_commands2 = [rl_command0_1, rl_command0_2, rl_command1]
-    rl_command_bezier2 = WxWebotsApi.line_fit(rl_commands2, timeline=1000000)
-    rl_command_bezier = WxWebotsApi.line_fit(rl_commands, timeline=1000000)
+    ll_commands = [ll_command1, ll_command2, ll_command3]
+    ll_command_bezier = WxWebotsApi.line_fit(ll_commands, timeline=500000)
+    rl_command_bezier2 = WxWebotsApi.line_fit(rl_commands2, timeline=100000)
+    rl_command_bezier = WxWebotsApi.line_fit(rl_commands, timeline=800000)
     rl_is_over = False
     rl2_is_over = False
     rh1_is_over = False
@@ -84,6 +87,11 @@ def task10():
     rh3_is_over = False
     rh4_is_over = False
     rh5_is_over = False
+    rh6_is_over = False
+    ll_is_over = False
+    ll_names = ['LShoulderPitch', 'LShoulderRoll', 'LShoulderYaw',
+                'LElbowRoll', 'LElbowYaw',
+                'LWristPitch', 'LWristRoll']
     if ws.walk_forward_step(step_num=1, linear_x=-0.30):
         while not rospy.is_shutdown():
             # l_command[2] = 1.85  # LShoulderPitch
@@ -132,12 +140,17 @@ def task10():
                 while (last - first) < 0.45:
                     last = rospy.get_time()
                 rl_controller(rl_names, rl_command0_0, mode=5)
-                time.sleep(1)
+                rh4_is_over = True
+            elif not ll_is_over:
+                for cmd in ll_command_bezier:
+                    ll_controller(ll_names, cmd, mode=5)
+                ll_is_over = True
+            elif not rh6_is_over:
                 for i in range(1, 60000):
                     rh_controller(rh_names, rh_command1, mode=5)
                 for i in range(1, 60000):
                     rh_controller(rh_names, rh_command2, mode=5)
-                rh4_is_over = True
+                rh6_is_over = True
             elif not rh5_is_over:
                 for cmd in rl_command_bezier2:
                     rl_controller(rl_names, cmd, mode=5)
@@ -147,25 +160,12 @@ def task10():
     '''步态旋转'''
 
 
-    ws.start()
-    first = rospy.get_time()
-    last = rospy.get_time()
-    ws.vel(angular_z=0.3)
-    while (last - first)<4.8:
-        ws.vel(angular_z=-0.3)
-        last = rospy.get_time()
-    ws.stop()
+    # ws.start()
+    # first = rospy.get_time()
+    # last = rospy.get_time()
+    # ws.vel(angular_z=0.3)
+    # while (last - first)<4.5:
+    #     ws.vel(angular_z=-0.2)
+    #     last = rospy.get_time()
+    # ws.stop()
 
-
-            # for i in range(1,300000):
-            #     r_command[0] = -1.6
-            #     rl_controller(r_names, r_command)
-            # for i in range(1,300000):
-            #     r_command[1] = 0.1
-            # for i in range(1,300000):
-            #     r_command[3] = -1
-            #     rl_controller(r_names, r_command)
-            # l_command = [0.9,-0.2,-1.8,-1.2,2.3,0,-0.35]
-            # 传入参数控制左臂
-        # while True:
-        #     if
